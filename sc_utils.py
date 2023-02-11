60e9c90de83c0c6b4f17b8c931105cb8861f106d
arashayat <arashayat@gmail.com>
2023-02-11 20:23:44 +0330

    second commit

--- sc_utils.py
+++ sc_utils.py
@@ -1,6 +1,8 @@
 
 import numpy as np
 from keras.utils import to_categorical
+import keras.backend as K
+
  
 def preprocess_data(dataset, vocab, T):
     '''
@@ -56,3 +58,24 @@ def string_to_int(string, length, vocab):
     
     #print (rep)
     return rep
+
+
+def softmax(x, axis=1):
+    """Softmax activation function.
+    # Arguments
+        x : Tensor.
+        axis: Integer, axis along which the softmax normalization is applied.
+    # Returns
+        Tensor, output of softmax transformation.
+    # Raises
+        ValueError: In case `dim(x) == 1`.
+    """
+    ndim = K.ndim(x)
+    if ndim == 2:
+        return K.softmax(x)
+    elif ndim > 2:
+        e = K.exp(x - K.max(x, axis=axis, keepdims=True))
+        s = K.sum(e, axis=axis, keepdims=True)
+        return e / s
+    else:
+        raise ValueError('Cannot apply softmax to a tensor that is 1D')
